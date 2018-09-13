abef0eda7c69cbe514e4d35dc819da4b9c2e3ab0
KJ Wilson <yourboykjdev@gmail.com>
2018-09-13 11:01:43 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express()
 // express handlebars
 var exphbs = require('express-handlebars');
 
+// arrays for reviews
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+]
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -14,6 +20,12 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     // extend roote route from views
     res.render('home', { msg: 'Hello World!' });
+})
+
+// index
+// sends object (review arrays) to views for rendering
+app.get('/reviews', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
 })
 
 //server
