32bd966f5a83923226052cd2a5c91420bd491a8f
sad boy sad boy <yourboykjdev@gmail.com>
2018-10-29 17:08:56 -0700

    second attempt in the works

--- app.js
+++ app.js
@@ -1,69 +1,23 @@
-// RottenPotatoes
-//
-// EXPRESS
-//
-// express
-// a light-weight web application framework
-// organizes resources into an MVC architecture
-//
-// app
-//
-// body parser
-// parse - to analyze into parts and roles
-// bodyParser parses your request and converts
-// the data into an easily extractable format
-// imports req.body and req
-//
-// method-override
-// for server to get "PUT" command
-// since HTML doesnt have it
-//
-// express handlebars
-// handlebars is templating engine
-//
-// import reviews.js
-//
+// Express
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-//
-// DATABASE
-//
-// connect to mongo database
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/rottenpotatoes')
-//
-//
-//
-// Routers
-// arrays for reviews
-// let reviews = [
-//     { title: "Great Review" },
-//     { title: "Next Review" }
-// ]
-//
-// Below is a route to the reviews model in the controllers
-const reviews = require('./controllers/reviews')
-// Handlebar Setup
-var exphbs = require('express-handlebars')
 
-// // arrays for reviews
-// let reviews = [
-//     { title: "Great Review" },
-//     { title: "Next Review" }
-// ]
+// Handlebars - views
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-// bodyParser update
-// app.use(bodyParser.urlencoded({ endcoded: true }))
-app.use(bodyParser.urlencoded({ extended: true }))
-// overrides with POST
-app.use(methodOverride('_method'))
+// Body-Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app;
-//server
-app.listen(3000, () => {
-    console.log('App listening on port 3000!')
-});
+const port = 3000
+
+app.get('/', (req, res) => {
+    // res.send('Lets get this bread')
+    res.render('home', { msg: 'Handlebars enable us to get bread'})
+})
+
+app.listen(port, () => {
+    console.log('Boys, were up and running')
+})
