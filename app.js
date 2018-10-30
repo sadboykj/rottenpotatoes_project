d6697e92c22a65a88e142052cc071d90d5a38f8d
sad boy sad boy <yourboykjdev@gmail.com>
2018-10-29 17:20:11 -0700

    users can see all reviews 2

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-// Express
+// EXPRESS
 const express = require('express')
 const app = express()
 
-// Handlebars - views
+// HANDLEBARS - views
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Body-Parser
+// BODY-PARSER
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000
 
+// list of objects
+let reviews = [
+    { title: "Unexpected Best Movie Ever",
+      movieTitle: "The Room" },
+    { title: "Live-Action Anime Should Be Illegal",
+      movieTitle: "The Last Airbender"}
+]
+
+//ROUTERS
+
+// app.get('/', (req, res) => {
+//     // res.send('Lets get this bread')
+//     res.render('home', { msg: 'Handlebars enable us to get bread'})
+// })
+
 app.get('/', (req, res) => {
-    // res.send('Lets get this bread')
-    res.render('home', { msg: 'Handlebars enable us to get bread'})
+    res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(port, () => {
